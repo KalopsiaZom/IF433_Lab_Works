47ce5c42734cb163b91119c63a5bd5ce6d4b27ff
KalopsiaZom <bryant.marchianno@gmail.com>
2026-02-27 07:12:46 +0700

    week04: (task 2) create parent class Employee

---
+++ src/oop_00000116382_bryantmarchiannoj/week04/Employee.kt
@@ -0,0 +1,13 @@
+package oop_00000116382_bryantmarchiannoj.week04
+
+open class Employee(
+    val name: String,
+    val baseSalary: Int
+) {
+    open fun work(){
+        println("$name sedang bekerja.")
+    }
+    open fun calculateBonus() : Int{
+        return (baseSalary*10) / 100
+    }
+}
