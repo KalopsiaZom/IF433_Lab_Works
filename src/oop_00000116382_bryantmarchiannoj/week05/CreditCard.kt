623c3a2da0c5b1f7d4bc3934edcddcbf4e6cd28d
KalopsiaZom <bryant.marchianno@gmail.com>
2026-03-06 10:50:35 +0700

    week05: (task 2)create CreditCard class

---
+++ src/oop_00000116382_bryantmarchiannoj/week05/CreditCard.kt
@@ -0,0 +1,16 @@
+package oop_00000116382_bryantmarchiannoj.week05
+
+class CreditCard(
+    accountName: String,
+    val limit: Double,
+    var usedAmount: Double = 0.0
+    ) : PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if (usedAmount + amount < limit) {
+            usedAmount += amount
+            println("Pembayaran selesai. Total saldo terpakai: $usedAmount")
+        } else{
+            println("Pembayaran ditolak. Kartu melebihi limit.")
+        }
+    }
+}
