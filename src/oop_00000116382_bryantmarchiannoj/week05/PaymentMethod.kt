2e5101f2aed23827f2d23c0ab106659d9a78a1fb
KalopsiaZom <bryant.marchianno@gmail.com>
2026-03-06 07:29:00 +0700

    week05: (task 2) create EWallet class

--- src/oop_00000116382_bryantmarchiannoj/week05/PaymentMethod.kt
+++ src/oop_00000116382_bryantmarchiannoj/week05/PaymentMethod.kt
@@ -1,5 +1,5 @@
 package oop_00000116382_bryantmarchiannoj.week05
 
 abstract class PaymentMethod(val accountName: String) {
-    abstract fun processPayment(amount: Double): Double
+    abstract fun processPayment(amount: Double)
 }
