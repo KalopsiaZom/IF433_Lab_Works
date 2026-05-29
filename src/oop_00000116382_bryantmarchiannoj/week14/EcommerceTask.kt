95eab111b4c4b3daa4d74daab165dc053c32f9a4
KalopsiaZom <bryant.marchianno@gmail.com>
2026-05-29 09:25:23 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_00000116382_bryantmarchiannoj/week14/EcommerceTask.kt
+++ src/oop_00000116382_bryantmarchiannoj/week14/EcommerceTask.kt
@@ -1,12 +1,33 @@
 package oop_00000116382_bryantmarchiannoj.week14
 import java.io.File
+import java.io.FileWriter
 
-class BadOrderProcessor {
-    // VIOLATION: Hardcoded File I/O (DIP), Melakukan kalkulasi I/O Notifikasi sekali
+interface OrderRepository {
+    fun saveOrder(itemName: String, finalPrice: Double, customerType: String)
+}
+
+class CsvOrderRepository : OrderRepository {
+    override fun saveOrder(itemName: String, finalPrice: Double, customerType: String) {
+        FileWriter("orders.csv", true).use { writer ->
+            writer.append("\$itemName, \$finalPrice, \$customerType\n")
+        }
+    }
+}
+
+interface NotificationService {
+    fun sendNotification(message: String)
+}
+
+class EmailNotifier : NotificationService {
+    override fun sendNotification(message: String) {
+        println("Email terkirim: \$message")
+    }
+}
+
+/*class BadOrderProcessor {
     private val file = File("orders.csv")
 
     fun processOrder(itemName: String, basePrice: Double, customerType: String) {
-        // VIOLATION: Kaku jika ada tipe customer/diskon baru di masa depan (OCP)
         val finalPrice = when (customerType) {
             "REGULAR" -> basePrice
             "VIP" -> basePrice * 0.90 // Diskon 10%
@@ -14,10 +35,13 @@ class BadOrderProcessor {
         }
         println("Memproses pesanan \$itemName seharga \$finalPrice")
 
-        // VIOLATION SRP/DIP: Menulis file langsung di class bisnis
         file.appendText("\$itemName, \$finalPrice, \$customerType\n")
 
-        // VIOLATION SRP/DIP: Notifikasi terikat kuat dengan sistem order
         println("Email terkirim: Pesanan \$itemName Anda telah dikonfirmasi!")
     }
-}
+}*/
+
+class SafeOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService
+)
